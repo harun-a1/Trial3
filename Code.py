4bb529068325b70d3d8c90cb115cb45f6437344e
harun-a1 <43985436+harun-a1@users.noreply.github.com>
2019-11-09 11:01:39 +0300

    Create Code.py

---
+++ Code.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 28 11:08:42 2019
+@author: harundemir
+"""
+
+import requests
+import socket
+import sys
+from flask import Flask
+
+
+# If `entrypoint` is not defined in app.yaml, App Engine will look for an app
+# called `app` in `main.py`.
+app = Flask(__name__)
+
+
+@app.route('/')
+
+def func():
+    
+    response = requests.get('https://api.chucknorris.io/jokes/random')
+
+    responseCode = response.status_code
+    responseJoke = response.json()['value']
+
+    all_freq = {} 
+  
+    for i in responseJoke: 
+        if i in all_freq: 
+            all_freq[i] += 1
+        else: 
+            all_freq[i] = 1
+
+    Joke = responseJoke
+    result = str(all_freq)
+    return Joke + ' \n Counts: ' + result
+
+if __name__ == '__main__':
+    # This is used when running locally only. When deploying to Google App
+    # Engine, a webserver process such as Gunicorn will serve the app. This
+    # can be configured by adding an `entrypoint` to app.yaml.
+    app.run()
