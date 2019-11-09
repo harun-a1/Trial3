24e21c1cd8ddd498a777423d390a8718c2d34e2c
harun-a1 <43985436+harun-a1@users.noreply.github.com>
2019-11-09 11:13:00 +0300

    Update Code.py

--- Code.py
+++ Code.py
@@ -2,21 +2,17 @@
 # -*- coding: utf-8 -*-
 """
 Created on Mon Oct 28 11:08:42 2019
+
 @author: harundemir
 """
 
 import requests
-import socket
-import sys
 from flask import Flask
 
 
 # If `entrypoint` is not defined in app.yaml, App Engine will look for an app
 # called `app` in `main.py`.
-app = Flask(__name__)
 
-
-@app.route('/')
 
 def func():
     
@@ -37,8 +33,4 @@ def func():
     result = str(all_freq)
     return Joke + ' \n Counts: ' + result
 
-if __name__ == '__main__':
-    # This is used when running locally only. When deploying to Google App
-    # Engine, a webserver process such as Gunicorn will serve the app. This
-    # can be configured by adding an `entrypoint` to app.yaml.
-    app.run()
+func()
